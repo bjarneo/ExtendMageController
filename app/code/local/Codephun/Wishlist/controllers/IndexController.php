8d197e2282b40de4d148e8b4c232efc4c404e8d6
bjarneo <bjarne.oeverli@gmail.com>
2014-02-27 11:44:17 +0100

    Extend Core Controller

--- app/code/local/Codephun/Wishlist/controllers/IndexController.php
+++ app/code/local/Codephun/Wishlist/controllers/IndexController.php
@@ -10,10 +10,10 @@
 require_once Mage::getModuleDir('controllers', 'Mage_Wishlist') . DS . 'IndexController.php';
 class Codephun_Wishlist_IndexController extends Mage_Wishlist_IndexController
 {
-    public function sendAction()
+    public function indexAction()
     {
         echo 'If I want my code to run before parent sendAction';
         // If you don't want to use parent code, just don't add parent::methodName.
-        parent::sendAction();
+        parent::indexAction();
     }
 }
